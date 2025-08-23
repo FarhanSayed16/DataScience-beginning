e246477a5a3ba90c8b6e0effd330141d8556547f
Farhan <farhansayed54@gmail.com>
2025-08-24 03:28:48 +0530

    u0pdate

---
+++ SQL/Day5/General order.sql
@@ -0,0 +1,12 @@
+-- select column(s)
+-- from table_name
+-- where condition;
+-- group by column(s);
+-- having condition;
+-- order by column(s) asc|desc;
+
+
+
+---------imp-----
+
+-- where apply condition to `rows` and having apply `condition` to groups.
