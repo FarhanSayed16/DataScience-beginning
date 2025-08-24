40a864e1aececafe57e0a16d2ab2daf08536ffcf
Farhan <farhansayed54@gmail.com>
2025-08-24 23:01:26 +0530

    truncate

---
+++ SQL/Day5/truncate.sql
@@ -0,0 +1,4 @@
+truncate table courses;
+-- what this does?
+-- This command removes all rows from the courses table, but keeps the table structure intact.
+-- so if you use drop table, the whole data is deleted with the table structure and the rows and parameters. but truncate keep the parameters and only delete the data inside it.
