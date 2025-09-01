b2f5fb36e39f5fd0ab64811727c6d7d9e8636b4e
Farhan <farhansayed54@gmail.com>
2025-09-01 23:58:23 +0530

    casting in python

---
+++ python/Day1 copy/casting.py
@@ -0,0 +1,31 @@
+# Python Casting Examples
+
+# Integer casting
+x = int(1)        # x will be 1
+y = int(2.8)      # y will be 2
+z = int("3")      # z will be 3
+
+# Float casting
+a = float(1)      # a will be 1.0
+b = float("2.5")  # b will be 2.5
+c = float(3)      # c will be 3.0
+
+# String casting
+s1 = str(10)      # s1 will be '10'
+s2 = str(2.5)     # s2 will be '2.5'
+s3 = str(True)    # s3 will be 'True'
+
+print(x, y, z)
+print(a, b, c)
+print(s1, s2, s3)
+# Explanation of casting and types
+
+# In Python, a "type" refers to the kind of value a variable holds, such as integer (int), floating-point number (float), or string (str).
+# "Casting" means converting a value from one type to another.
+# For example, you can cast a float to an int, or an int to a string.
+# This is useful when you need a value in a specific type for calculations or display.
+
+# Example:
+# int("5") converts the string "5" to the integer 5.
+# float(3) converts the integer 3 to the floating-point number 3.0.
+# str(7) converts the integer 7 to the string "7".
