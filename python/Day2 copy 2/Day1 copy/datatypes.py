6471675728e0c9348b60c6a464aaaa27d51c311e
Farhan <farhansayed54@gmail.com>
2025-09-04 23:55:27 +0530

    Add example demonstrating basic data types in Python

---
+++ python/Day2 copy 2/Day1 copy/datatypes.py
@@ -0,0 +1,43 @@
+# Basic Data Types in Python
+
+# Integer
+a = 10
+print(type(a))  # <class 'int'>
+
+# Float
+b = 3.14
+print(type(b))  # <class 'float'>
+
+# String
+c = "Hello, Python!"
+print(type(c))  # <class 'str'>
+
+# Boolean
+d = True
+print(type(d))  # <class 'bool'>
+
+# List
+e = [1, 2, 3, 4]
+print(type(e))  # <class 'list'>
+
+# Tuple
+f = (5, 6, 7)
+print(type(f))  # <class 'tuple'>
+
+# Dictionary
+g = {'name': 'Alice', 'age': 25}
+print(type(g))  # <class 'dict'>
+
+# Set
+h = {1, 2, 3}
+print(type(h))  # <class 'set'>
+
+# Meaning and Uses:
+# int: Integer numbers, used for counting and indexing.
+# float: Decimal numbers, used for measurements and calculations with fractions.
+# str: Text data, used for storing and manipulating words, sentences, etc.
+# bool: Boolean values, used for logical operations and conditions (True/False).
+# list: Ordered, mutable collection, used for storing sequences of items.
+# tuple: Ordered, immutable collection, used for fixed sequences of items.
+# dict: Key-value pairs, used for mapping and fast lookups.
+# set: Unordered collection of unique items, used for membership tests and removing duplicates.
